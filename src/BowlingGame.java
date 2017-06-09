e5288d85775f5afce663cde0f0fd4c3ad773544d
somename <someemail>
2017-06-09 15:14:53 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -9,8 +9,11 @@ public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
+	private int totalScore = 0;
 	
-	public BowlingGame(){}
+	public BowlingGame(){
+		
+	}
 	
 	// adds a frame to the game
 	public void addFrame(Frame frame) throws BowlingException{
@@ -22,13 +25,29 @@ public class BowlingGame {
 	
 	// Sets the bonus throws at the end of the game
 	public void setBonus(int firstThrow, int secondThrow) {
-		//to be implemented
+		bonus = new Frame(firstThrow, secondThrow);
 	}
 	
 	// Returns the game score
-	public int score(){
-		//to be implemented: should return game score 
-		return 0;
+	public int score() {
+		for (int i = 0; i < frames.size(); i++) {
+			if (i != 0) {
+				if(frames.get(i-1).isStrike()) {
+					frames.get(i-1).setFrameScore(frames.get(i-1).getFrameScore() + frames.get(i).getFrameScore());
+				} else if (frames.get(i-1).isSpare()) {
+					frames.get(i-1).setFrameScore(frames.get(i-1).getFrameScore() + frames.get(i).getFirstThrow());
+				}
+				totalScore += frames.get(i).getFrameScore();
+			}
+		}
+		if (bonus != null) {
+			if (frames.get(9).isStrike()) {
+				totalScore += bonus.getFrameScore();
+			} else if (frames.get(9).isSpare()) {
+				totalScore += bonus.getFirstThrow();
+			}
+		}
+		return totalScore;
 	}
 
 	public List<Frame> getFrames() {
