18010260bc2b724d7671b32fae431310e920534a
somename <someemail>
2017-06-09 14:38:18 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -21,14 +21,26 @@ public class TestBowling {
 	
 	@Test
 	public void testIfFrameIsStrike() {
-		fr = new Frame(10, 0);
+		fr = new Frame(10,0);
 		assertTrue(fr.isStrike());
 	}
 	
 	@Test
 	public void testIfFrameIsNotStrike() {
-		fr = new Frame(4, 0);
+		fr = new Frame(4,0);
 		assertFalse(fr.isStrike());
+	}
+	
+	@Test
+	public void testIfFrameIsSpare() {
+		fr = new Frame(2,8);
+		assertTrue(fr.isSpare());
+	}
+	
+	@Test
+	public void testIfFrameIsNotSpare() {
+		fr = new Frame(2,3);
+		assertFalse(fr.isSpare());
 	}
 
 }
